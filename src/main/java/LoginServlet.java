a69c205ff61418851b2a478cdf983262ddfbdc88
Michael Dickens <dickens.michael64@gmail.com>
2022-07-14 15:10:52 -0500

    push session and cookies upates

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -16,7 +17,9 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+
         if (validAttempt) {
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
---
+++ src/main/webapp/WEB-INF/logout.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: michaeldickens
+  Date: 7/14/22
+  Time: 11:29 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+
+</head>
+<body>
+
+<jsp:include page="/partials/navbar.jsp" />
+
+<div class="container">
+    <h1>Logged out.</h1>
+</div>
+
+</body>
+</html>
