c0147bbac85426a2681818471613673432bd0ca6
Michael Dickens <dickens.michael64@gmail.com>
2022-08-12 17:07:04 -0500

    rebuilding jdbc exercise

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,11 +1,13 @@
 public class DaoFactory {
     private static Ads adsDao;
-    static Config config = new Config();
+
+    private static Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new MySQLDao(config);
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
+
 }
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,52 @@
+import com.mysql.cj.jdbc.Driver;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
+        } catch (SQLException sqle) {
+            throw new RuntimeException("Error connecting to db", sqle);
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM adlister_db.ads");
+            while (rs.next()) {
+                Ad ad = new Ad(rs.getLong("id"), rs.getLong("user_id"), rs.getString("title"), rs.getString("description"));
+                ads.add(ad);
+            }
+        } catch (SQLException sqle) {
+            throw new RuntimeException("Error connecting to db", sqle);
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        Long user_id = 1L;
+        String title = ad.getTitle();
+        String description = ad.getDescription();
+
+        String query = "INSERT INTO adlister_db.ads (user_id, title, description) VALUES ('" + user_id + "', '" + title + "', '" + description + "')";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            long key = rs.getLong(1);
+        } catch (SQLException sqle){
+            throw new RuntimeException("Error connecting to db", sqle);
+        }
+        return null;
+    }
+}
